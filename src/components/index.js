3c52b3f0abd9c754df46a39fd9f3cc3c0fbcc384
Nerilus <nerilus.h@gmail.com>
2023-07-13 13:50:40 +0200

    Create-users-page

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import SideBar from './SideBar/SideBar'
 import MyComponent from './MyComponent/MyComponent'
-import Add from './Add/Add'
 import DataTable from './DataTable/DataTable'
 
 export {
   MyComponent,
   SideBar,
-  Add,
   DataTable,
 }
