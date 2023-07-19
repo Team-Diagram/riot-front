fbcb74d856d74c39f53888747ba32b445d3c1c2f
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-07-19 21:20:25 +0200

    feat : add modal user and correction sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,11 @@ import Add from './Add/Add'
 import SelectInput from './SelectInputs/SelectInput'
 import SideBar from './SideBar/SideBar'
 import TableGlobal from './TableGlobal/TableGlobal'
-
+import ModalUser from './Modal/ModalUser'
 export {
   Add,
   SelectInput,
   SideBar,
   TableGlobal,
+  ModalUser,
 }
