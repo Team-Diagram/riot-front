53078da34f74788f0ab6dacf66c6350503bf8cae
Nerilus <nerilus.h@gmail.com>
2023-07-19 16:23:47 +0200

    Fix sidebar role

--- src/views/Users/Users.jsx
+++ src/views/Users/Users.jsx
@@ -1,11 +1,11 @@
-import { Button,Select, SelectItem } from '@tremor/react'
-import { PlusCircleIcon } from '@heroicons/react/solid' 
+import { Button, Select, SelectItem } from '@tremor/react'
+import { PlusCircleIcon } from '@heroicons/react/solid'
 import React, { useEffect, useState } from 'react'
-import {SelectInput } from 'src/components'
+import { SelectInput } from 'src/components'
 
 function Users() {
   const [utilisateurs, setUtilisateurs] = useState([])
-  const [role,setRole] = useState([])
+  const [role, setRole] = useState([])
   const [loading, setLoading] = useState(true)
   const [nouvelUtilisateur, setNouvelUtilisateur] = useState({
     first_name: '',
@@ -61,7 +61,6 @@ function Users() {
       })
   }
 
-
   const handleAddUser = () => {
     const token = localStorage.getItem('token')
     const apiUrl = 'http://localhost:8787/api/user'
@@ -94,36 +93,35 @@ function Users() {
         window.location.reload()
       })
   }
-  const handleRoleChange = (selectedRole,uuid) => {
+  const handleRoleChange = (selectedRole, uuid) => {
     const token = localStorage.getItem('token')
-    let isAdmin = false;
-    let role = ['ROLE_USER'];
-    
-    if (selectedRole === "Admin") {
-      isAdmin = true;
-      role = ['ROLE_ADMIN'];
+    let isAdmin = false
+    let role = ['ROLE_USER']
+
+    if (selectedRole === 'Admin') {
+      isAdmin = true
+      role = ['ROLE_ADMIN']
     }
-    
+
     fetch(`http://localhost:8787/api/user/update/${uuid}`, {
       method: 'PUT',
       body: JSON.stringify({
         roles: role,
-        admin: isAdmin
+        admin: isAdmin,
       }),
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
-      }
+      },
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         window.location.reload()
       })
-      .catch(error => {
-        console.error(error);
-      });
-    
-  };
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -159,100 +157,100 @@ function Users() {
                 &times;
               </span>
             </div>
-              <label htmlFor="firstNameInput">
-                Prénom:
-                <input
-                  type="text"
-                  name="first_name"
-                  placeholder='Anthony'
-                  value={nouvelUtilisateur.first_name}
-                  onChange={handleChange}
-                />
-              </label>
-              
-              <label htmlFor="lastNameInput">
-                Nom:
-                <input
-                  type="text"
-                  placeholder='Ringressi'
-                  name="last_name"
-                  value={nouvelUtilisateur.last_name}
-                  onChange={handleChange}
-                />
-              </label>
+            <label htmlFor="firstNameInput">
+              Prénom:
+              <input
+                type="text"
+                name="first_name"
+                placeholder="Anthony"
+                value={nouvelUtilisateur.first_name}
+                onChange={handleChange}
+              />
+            </label>
 
-              <label htmlFor="emailInput">
-                Adresse e-mail:
-                <input
-                  type="email"
-                  name="email"
-                  placeholder='exemple@gmail.com'
-                  value={nouvelUtilisateur.email}
-                  onChange={handleChange}
-                />
-              </label>
+            <label htmlFor="lastNameInput">
+              Nom:
+              <input
+                type="text"
+                placeholder="Ringressi"
+                name="last_name"
+                value={nouvelUtilisateur.last_name}
+                onChange={handleChange}
+              />
+            </label>
 
-              <label htmlFor="passwordInput">
-                Mot de passe:
-                <input
-                  type="password"
-                  name="password"
-                  placeholder='••••••••••'
-                  value={nouvelUtilisateur.password}
-                  onChange={handleChange}
-                />
-              </label>
+            <label htmlFor="emailInput">
+              Adresse e-mail:
+              <input
+                type="email"
+                name="email"
+                placeholder="exemple@gmail.com"
+                value={nouvelUtilisateur.email}
+                onChange={handleChange}
+              />
+            </label>
 
-              <label htmlFor="adminInput" className='adminCheckbox'>
-                Cocher pour rendre admin:
-                <input
-                  type="checkbox"
-                  name="admin"
-                  checked={nouvelUtilisateur.admin}
-                  onChange={handleChange}
-                />
-              </label>
+            <label htmlFor="passwordInput">
+              Mot de passe:
+              <input
+                type="password"
+                name="password"
+                placeholder="••••••••••"
+                value={nouvelUtilisateur.password}
+                onChange={handleChange}
+              />
+            </label>
 
-              <Button variant="primary" onClick={handleAddUser}>
-                Ajouter un utilisateur
-              </Button>
+            <label htmlFor="adminInput" className="adminCheckbox">
+              Cocher pour rendre admin:
+              <input
+                type="checkbox"
+                name="admin"
+                checked={nouvelUtilisateur.admin}
+                onChange={handleChange}
+              />
+            </label>
+
+            <Button variant="primary" onClick={handleAddUser}>
+              Ajouter un utilisateur
+            </Button>
           </div>
         </div>
       )}
       <div className="table-container">
-      <table className='table-user'>
-        <thead>
-          <tr>
-            <th className='table-user__header-cell'>Rôle</th>
-            <th className='table-user__header-cell'>Prénom</th>
-            <th className='table-user__header-cell'>Nom</th>
-            <th className='table-user__header-cell'>Adresse e-mail</th>
-            <th className='table-user__header-cell'></th>
-          </tr>
-        </thead>
-        <tbody>
-          {utilisateurs.map((utilisateur) => (
-            <tr key={utilisateur.email} className='table-user__row'>
-              <td className='table-user__cell'>
-                <SelectInput
-                  onChange={handleRoleChange}
-                  uuid={utilisateur.id}
-                  placeholder={utilisateur.roles[0] === 'ROLE_ADMIN' ? 'Admin' : 'User'}
-                  data={['Admin', 'User']}
-                />
-              </td>
-              <td className='table-user__cell'>{utilisateur.firstName}</td>
-              <td className='table-user__cell'>{utilisateur.lastName}</td>
-              <td className='table-user__cell'>{utilisateur.email}</td>
-              <td className='table-user__cell'>
-                <Button variant="secondary" color="red" onClick={() => handleDeleteUser(utilisateur.id)}>
-                  Supprimer
-                </Button>
-              </td>
+        <table className="table-user">
+          <thead>
+            <tr>
+              <th className="table-user__header-cell">Rôle</th>
+              <th className="table-user__header-cell">Prénom</th>
+              <th className="table-user__header-cell">Nom</th>
+              <th className="table-user__header-cell">Adresse e-mail</th>
+              <th className="table-user__header-cell" />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {utilisateurs.map((utilisateur) => (
+              <tr key={utilisateur.email} className="table-user__row">
+                <td className="table-user__cell">
+                  <SelectInput
+                    onChange={handleRoleChange}
+                    uuid={utilisateur.id}
+                    placeholder={utilisateur.roles[0] === 'ROLE_ADMIN' ? 'Admin' : 'User'}
+                    data={['Admin', 'User']}
+                  />
+                </td>
+                <td className="table-user__cell">{utilisateur.firstName}</td>
+                <td className="table-user__cell">{utilisateur.lastName}</td>
+                <td className="table-user__cell">{utilisateur.email}</td>
+                <td className="table-user__cell">
+                  <Button variant="secondary" color="red" onClick={() => handleDeleteUser(utilisateur.id)}>
+                    Supprimer
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
