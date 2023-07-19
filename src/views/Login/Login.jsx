8cf813f8b47a1d48d014c10945b365cf8c13cdde
Nerilus <91333750+Nerilus@users.noreply.github.com>
2023-07-19 15:39:39 +0200

    Add routes protected (#63)

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../AuthContext/AuthContext'
 
@@ -6,11 +6,22 @@ function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState(null)
-  const { login, isAuthenticated, logout } = useContext(AuthContext)
+  const { login, isAuthenticated } = useContext(AuthContext)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    // If the user is already authenticated, redirect to the home page
+    if (isAuthenticated) {
+      navigate('/')
+    }
+  }, [isAuthenticated, navigate])
 
   async function handleSubmit(event) {
     event.preventDefault()
+    if (!email || !password) {
+      setError('Veuillez remplir tout les champs.')
+      return
+    }
     try {
       await login(email, password)
       navigate('/')
@@ -19,52 +30,41 @@ function Login() {
     }
   }
 
-  const handleLogout = () => {
-    logout()
-  }
-
   return (
     <div className="login">
-      {isAuthenticated ? (
-        <>
-          <p>tu es connecté</p>
-          <button type="button" onClick={handleLogout}>logout</button>
-        </>
-      ) : (
-        <div className="login-container">
-          <div className="box-shadow-1 box-model">
-            <h1 className="text-center">Login</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="fields-column">
-                <div className="fields-row">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="adresse mail"
-                    name="username"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="fields-row">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="mot de passe"
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
+      <div className="login-container">
+        <div className="box-shadow-1 box-model">
+          <h1 className="text-center">Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="fields-column">
+              <div className="fields-row">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="adresse mail"
+                  name="username"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
-              <div className="bloc-btn">
-                {error && <p className="error">{error}</p>}
-                <button type="submit">je me connecte</button>
+              <div className="fields-row">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="mot de passe"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
-            </form>
-          </div>
+            </div>
+            <div className="bloc-btn">
+              {error && <p className="error">{error}</p>}
+              <button type="submit">je me connecte</button>
+            </div>
+          </form>
         </div>
-      )}
+      </div>
     </div>
 
   )
